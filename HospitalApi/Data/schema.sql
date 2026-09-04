29c67602093f6d1bdf2044b58e68bb40755bd319
Knunez2711 <kevinne57@gmail.com>
2026-09-04 16:58:13 -0500

    Se implementan funciones para crear, obtener y actualizar pacientes, incluyendo manejo de duplicados y registro de historial de contacto.

--- HospitalApi/Data/schema.sql
+++ HospitalApi/Data/schema.sql
@@ -1,27 +1,102 @@
-CREATE TABLE IF NOT EXISTS pacientes
-(
-    id SERIAL PRIMARY KEY,
-    tipo_documento VARCHAR(50),
-    numero_documento VARCHAR(50),
-    nombre VARCHAR(150),
-    fecha_nacimiento DATE,
-    correo VARCHAR(100),
-    genero VARCHAR(10),
-    direccion VARCHAR(200),
-    telefono VARCHAR(20),
-    email VARCHAR(100),
-    activo BOOLEAN NOT NULL DEFAULT TRUE,
-    fecha_creacion TIMESTAMP NOT NULL DEFAULT NOW()
-);
+Hospital=# CREATE OR REPLACE FUNCTION crear_paciente (
+    p_tipo_documento VARCHAR,
+    p_numero_documento VARCHAR,
+    p_nombre VARCHAR,
+    p_fecha_nacimiento DATE,
+    p_correo_electronico VARCHAR,
+    p_genero VARCHAR,
+    p_direccion VARCHAR,
+    p_numero_telefono VARCHAR,
+    p_activo BOOLEAN
+)
+RETURNS SETOF pacientes AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM pacientes WHERE tipo_documento = p_numero_documento
+    )THEN
+        RAISE EXCEPTION 'DUPLICADO: ya existe un paciente con este tipo y numero de documento.';
+    END IF;
 
-CREATE UNIQUE INDEX IF NOT EXISTS ux_pacientes_tipo_numero_documento
-    ON pacientes (tipo_documento, numero_documento);
+    RETURN QUERY
+    INSERT INTO pacientes(
+        tipo_documento, numero_documento, nombre, fecha_nacimiento, correo, genero, direccion, telefono, email,
+        activo
+        )
+        VALUES (
+            p_tipo_documento, p_numero_documento, p_nombre, p_fecha_nacimiento, p_correo_electronico, p_genero,
+            p_direccion, p_numero_telefono, p_activo
+        )
+        RETURNING *;
+    END
+    $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS historial_contacto_paciente
-(
-    historial_id SERIAL PRIMARY KEY,
-    paciente_id INTEGER NOT NULL REFERENCES pacientes(id),
-    correo_anterior VARCHAR(255) NOT NULL,
-    telefono_anterior VARCHAR(20) NOT NULL,
-    fecha_cambio TIMESTAMP NOT NULL DEFAULT NOW()
-);
+
+CREATE OR REPLACE FUNCTION obtener_pacientes()
+RETURNS SETOF pacientes AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *FROM pacientes
+    ORDER BY id DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION obtener_pacientes_por_id(p_paciente_id INT)
+RETURNS SETOF pacientes AS $$
+BEGIN
+    RETURN QUERY
+    SELECT * FROM pacientes
+    WHERE id = p_paciente_id;
+END;
+$$ LANGUAGE plpgsql;
+
+/*
+actualizar paciente
+*/
+
+CREATE OR REPLACE FUNCTION actualizar_paciente(
+    p_paciente_id INT,
+    p_tipo_documento VARCHAR,
+    p_numero_documento VARCHAR,
+    p_nombre VARCHAR,
+    p_fecha_nacimiento DATE,
+    p_correo_electronico VARCHAR,
+    p_genero VARCHAR,
+    p_direccion VARCHAR,
+    p_numero_telefono VARCHAR,
+    p_activo BOOLEAN
+)
+RETURNS SETOF pacientes AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM pacientes WHERE id = p_paciente_id
+    )THEN
+        RAISE EXCEPTION 'NO ENCONTRADO: Paciente no encontrado.';
+    END IF;
+
+    IF EXISTS(
+        SELECT 1 FROM pacientes WHERE tipo_documento = p_tipo_documento  AND numero_documento = p_numero_documento
+        AND id <> p_paciente_id
+    )THEN
+        RAISE EXCEPTION 'DUPLICADO: Ya existe otro paciente con este tipo y numero de documento';
+    END IF;
+
+    INSERT INTO historial_contacto_paciente (paciente_id, correo_anterior, telefono_anterior)
+    SELECT id, correo_electronico, numero_telefono
+    FROM pacientes
+    WHERE id = p_pciente_id;
+
+    RETURN QUERY
+    UPDATE pacientes
+    SET tipo_documento = p_tipo_documento,
+        numero_documento = p_numero_documento,
+        nombre = p_nombre,
+        fecha_nacimiento = p_fecha_nacimiento,
+        correo = p_correo_electronico,
+        genero = p_genero,
+        direccion = p_direccion,
+        telefono = p_numero_telefono,
+        activo = p_activo
+    WHERE id = p_paciente_id
+    RETURNING *;
+END;
+$$ LANGUAGE plpgsql;
